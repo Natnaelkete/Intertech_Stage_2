53dec217b584c70473463419e57eecc87338bdff
Natnaelkete <ketemanatnael023@gmail.com>
2024-11-23 04:37:47 -0800

    add middleware

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ dotenv.config();
 
 import connectDB from "./config/DB.js";
 
+import booksRoute from "./routes/bookRoute.js";
+
 await connectDB();
 
 const PORT = process.env.PORT || 3000;
@@ -14,6 +16,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.use("/api", booksRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running ${PORT}`);
